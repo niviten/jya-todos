fa65ce944db74965e93224db8927cbf20f60f860
niviten <l.j.nivethan@gmail.com>
2024-01-20 23:08:30 +0530

    feat: Get todo by Id

--- src/components/todo/todo_services.go
+++ src/components/todo/todo_services.go
@@ -11,9 +11,9 @@ func Create(todo *Todo) (int64, error) {
 		return -1, err
 	}
 	query := `
-    INSERT INTO Todo (
-        todo_title, todo_description, created_at, due_by, priority
-    ) VALUES (?, ?, ?, ?, ?)
+        INSERT INTO Todo (
+            todo_title, todo_description, created_at, due_by, priority
+        ) VALUES (?, ?, ?, ?, ?)
     `
 	ctx := context.TODO()
 	result, err := db.ExecContext(
@@ -29,4 +29,31 @@ func Create(todo *Todo) (int64, error) {
 		return -1, err
 	}
 	return result.LastInsertId()
+}
+
+func GetById(todoId int64) (Todo, error) {
+	db, err := connection.GetDBConnection()
+	if err != nil {
+		return Todo{}, err
+	}
+	query := `
+        SELECT
+            todo_id, todo_title, todo_description, is_completed, created_at,
+            due_by, priority
+        FROM Todo
+        WHERE todo_id = ?
+    `
+
+	var t Todo
+	scanErr := db.
+		QueryRow(query, todoId).
+		Scan(&t.Id, &t.Title, &t.Description,
+			&t.IsCompleted, &t.CreatedAt, &t.DueBy,
+			&t.Priority,
+		)
+	if scanErr != nil {
+		return Todo{}, scanErr
+	}
+
+	return t, nil
 }
