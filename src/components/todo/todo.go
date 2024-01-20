40b1962e07b5b2fafbd4225fd149042ee984f790
niviten <l.j.nivethan@gmail.com>
2024-01-20 23:08:12 +0530

    feat: Add String method for Todo struct

--- src/components/todo/todo.go
+++ src/components/todo/todo.go
@@ -1,5 +1,10 @@
 package todo
 
+import (
+	"fmt"
+	"time"
+)
+
 type Todo struct {
 	Id          int64
 	Title       string
@@ -12,4 +17,17 @@ type Todo struct {
 
 func NewTodo() *Todo {
 	return &Todo{}
+}
+
+func (t *Todo) String() string {
+	completedStatus := "Incomplete"
+	if t.IsCompleted {
+		completedStatus = "Completed"
+	}
+
+	createdAtStr := time.Unix(t.CreatedAt, 0).Format("2006-01-02 15:04:05")
+	dueByStr := time.Unix(t.DueBy, 0).Format("2006-01-02 15:04:05")
+
+	return fmt.Sprintf("Todo ID: %d\nTitle: %s\nDescription: %s\nStatus: %s\nCreated At: %s\nDue By: %s\nPriority: %d",
+		t.Id, t.Title, t.Description, completedStatus, createdAtStr, dueByStr, t.Priority)
 }
